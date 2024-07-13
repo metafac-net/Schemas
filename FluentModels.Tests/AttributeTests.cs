b1718e67d4caf9888ba2d107e7ce0de3e6f8668e
lucian <lucian@metafac.net>
2024-07-13 22:15:27 +1000

    more unit tests

--- FluentModels.Tests/AttributeTests.cs
+++ FluentModels.Tests/AttributeTests.cs
@@ -6,21 +6,43 @@ using Xunit;
 
 namespace FluentModels.UnitTests
 {
-    [Entity("7311f284-ae1d-4d26-96f5-5e37f7a66c3e")] internal class GoodEntity { }
+    [Entity("7311f284-ae1d-4d26-96f5-5e37f7a66c3e")]
+    internal class EntityA
+    {
+        [Member(1)] public int Id { get; set; }
+    }
+
+    [Entity("d08e4ec2-917c-4bef-a6cf-133435e667ca", ItemState.Hidden, "Reserved")]
+    internal class EntityB
+    {
+        [Member(1)] public int Id { get; set; }
+    }
 
     public class AttributeTests
     {
-        [Fact]
-        public void NormalUsageTest()
+        private EntityAttribute GetEntityAttribute(Type type)
         {
-            Attribute[] customAttributes = typeof(GoodEntity).GetCustomAttributes().ToArray();
-            customAttributes.Length.Should().Be(1);
-
-            Attribute customAttribute = customAttributes[0];
+            Attribute customAttribute = type.GetCustomAttributes().Single();
             customAttribute.Should().BeOfType<EntityAttribute>();
+            return (EntityAttribute)customAttribute;
+        }
 
-            EntityAttribute entityAttribute = (EntityAttribute)customAttribute;
+        [Fact]
+        public void Entity01_Active()
+        {
+            EntityAttribute entityAttribute = GetEntityAttribute(typeof(EntityA));
             entityAttribute.UniqueId.Should().Be(new Guid("7311f284-ae1d-4d26-96f5-5e37f7a66c3e"));
+            entityAttribute.State.Should().Be(ItemState.Active);
+            entityAttribute.Reason.Should().BeNull();
+        }
+
+        [Fact]
+        public void Entity02_Hidden()
+        {
+            EntityAttribute entityAttribute = GetEntityAttribute(typeof(EntityB));
+            entityAttribute.UniqueId.Should().Be(new Guid("d08e4ec2-917c-4bef-a6cf-133435e667ca"));
+            entityAttribute.State.Should().Be(ItemState.Hidden);
+            entityAttribute.Reason.Should().Be("Reserved");
         }
     }
 }
